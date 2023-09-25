a21a576d6628626094c19898dad217fc8d2586c3
Biybinaz <turemuratova08@gmail.com>
2023-09-25 21:21:33 +0500

    все еще с ошибкой которую я не могу найти

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module uz.biybinaz.signlanguageapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires javafx.swing;
 
 
     opens uz.biybinaz.signlanguageapp to javafx.fxml;
